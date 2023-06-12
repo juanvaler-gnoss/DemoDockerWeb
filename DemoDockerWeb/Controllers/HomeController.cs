ad6fb3b79e81363dedef6dd7dd95167e4d55d2c1
Juan Valer - GNOSS <36291900+juanvaler-gnoss@users.noreply.github.com>
2023-06-12 16:55:10 +0200

    Initial commit

---
+++ DemoDockerWeb/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using DemoDockerWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoDockerWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string text = @"
+  _    _       _         _____  _       _                  _   _            
+ | |  | |     | |       |  __ \(_)     (_)                | | | |           
+ | |__| | ___ | | __ _  | |__) |_  ___  _  __ _ _ __   ___| |_| |_ ___  ___ 
+ |  __  |/ _ \| |/ _` | |  _  /| |/ _ \| |/ _` | '_ \ / _ \ __| __/ _ \/ __|
+ | |  | | (_) | | (_| | | | \ \| | (_) | | (_| | | | |  __/ |_| ||  __/\__ \
+ |_|  |_|\___/|_|\__,_| |_|  \_\_|\___/| |\__,_|_| |_|\___|\__|\__\___||___/
+                                      _/ |                                  
+                                     |__/                                   
+";
+
+            return Content(text, "text/plain");
+        }
+
+    }
+}
